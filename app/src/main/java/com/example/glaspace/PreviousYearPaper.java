1e4f7b78a38790da7bb6517a75bc2aa35e0a7be9
kb-s <kanhaiya.bhatt_cs18@gla.ac.in>
2021-05-06 22:59:29 +0530

    this is our previous year paper class

--- app/src/main/java/com/example/glaspace/PreviousYearPaper.java
+++ app/src/main/java/com/example/glaspace/PreviousYearPaper.java
@@ -9,11 +9,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class PreviousYearPaper extends AppCompatActivity {
     Button b1;
-    Spinner sp3, sp4;
+    Spinner sp3, sp4, sp5;
     String course[] = {"B.Tech", "BBA", "MBA", "Diploma", "Bsc"};
     String branch[] = {"CSE", "ME", "EE", "CE", "EE", "EC", "Finance", "Marketing", "Entrepreneurship", "Mathematics"};
+    String year[] = {"1st ", "2nd ", "3rd ", "4th "};
     ArrayAdapter<String> adp1;
     ArrayAdapter<String> adp2;
+    ArrayAdapter<String> adp3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +23,13 @@ public class PreviousYearPaper extends AppCompatActivity {
         setContentView(R.layout.activity_previous_year_paper);
         sp3 = findViewById(R.id.pyp1);
         sp4 = findViewById(R.id.pyp2);
+        sp5 = findViewById(R.id.pyp3);
         b1 = findViewById(R.id.search);
         adp1 = new ArrayAdapter<String>(PreviousYearPaper.this, android.R.layout.simple_list_item_1, course);
         adp2 = new ArrayAdapter<String>(PreviousYearPaper.this, android.R.layout.simple_list_item_1, branch);
+        adp3 = new ArrayAdapter<String>(PreviousYearPaper.this, android.R.layout.simple_list_item_1, year);
         sp3.setAdapter(adp1);
         sp4.setAdapter(adp2);
-
+        sp5.setAdapter(adp3);
     }
 }
--- app/src/main/java/com/example/glaspace/StudyMaterial.java
+++ app/src/main/java/com/example/glaspace/StudyMaterial.java
@@ -1,39 +1,33 @@
 package com.example.glaspace;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StudyMaterial extends AppCompatActivity {
-    Spinner sp1;
-    String courses[] = {"B.Tech", "BBA", "MBA", "Diploma", "Bsc"};
-    ArrayAdapter<String> adp;
+    Button button;
+    Spinner sp1, sp2;
+    String course1[] = {"B.Tech", "BBA", "MBA", "Diploma", "Bsc"};
+    String branch1[] = {"CSE", "ME", "EE", "CE", "EE", "EC", "Finance", "Marketing", "Entrepreneurship", "Mathematics"};
+
+    ArrayAdapter<String> adp4;
+    ArrayAdapter<String> adp5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_study_material);
+        setContentView(R.layout.activity_previous_year_paper);
         sp1 = findViewById(R.id.spin1);
-        adp = new ArrayAdapter<String>(StudyMaterial.this, android.R.layout.simple_list_item_1, courses);
-        sp1.setAdapter(adp);
-        sp1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                switch (i) {
-                    case 0:
-                        Spinner btech;
-                        String branches[] = {"CSE", "ME", "CE", "EE", "EC"};
-                        ArrayAdapter<String> badp;
-                        btech = findViewById(R.id.bTech);
-                        badp = new ArrayAdapter<String>(StudyMaterial.this, android.R.layout.simple_list_item_1, branches);
-                        btech.setAdapter(badp);
-
-                }
-            }
-        });
+        sp2 = findViewById(R.id.bTech);
+        button = findViewById(R.id.studysearch);
+        adp4 = new ArrayAdapter<String>(StudyMaterial.this, android.R.layout.simple_list_item_1, course1);
+        adp5 = new ArrayAdapter<String>(StudyMaterial.this, android.R.layout.simple_list_item_1, branch1);
+        sp1.setAdapter(adp4);
+        sp2.setAdapter(adp5);
     }
+
 }
